9f7c73712ee9810129c0d72da68f24d20d1a0c80
OUAKHILATOU <ouakhilatou@OUAKHILATOUs-MacBook-Pro.local>
2024-06-04 21:10:37 +0100

    dom manipulation

---
+++ cip.js
@@ -0,0 +1,16 @@
+const BoldCohortIsAmazing = true;
+const cakeIsGreat = false;
+console.log(BoldCohortIsAmazing);
+console.log(cakeIsGreat);
+
+
+const a = 12;
+let b = 24;
+var c = 30;
+
+const car = {
+    name: 'Renault',
+    colour: 'red' 
+}
+
+const colour = ['red', 'yellow', 'green', 'brown'];
---
+++ codeInPractice.js
@@ -0,0 +1,163 @@
+//console.log('It is working')
+//string
+// const nameOfTheCohort= "Bold";
+// // number
+// const number = 23;
+//Boolean value
+// const isBoldCohort = true;
+// const cakeIsGreat = false;
+// console.log(nameOfTheCohort);
+// console.log(number);
+// console.log(isBoldCohort);
+// console.log(cakeIsGreat);
+
+///////////VARIABLES///////
+// const a = 23;
+// console.log(a);
+// a = 45;
+// console.log(a);
+
+// let pname = "John";
+// console.log(pname);
+
+// var b = 9;
+// console.log('var a is: ' + b);
+//  b = 7;
+// console.log('var a is: ' + b);
+
+
+//// FUNCTIONS//////
+
+// function greeting() {
+//     console.log('Hello')
+// }
+
+// greeting();
+
+// function greeting(name) {
+//    console.log('Hello ' + name) 
+// }
+
+// greeting('Patience');
+// greeting('Ouakhila');
+
+///Function
+// function greeting (name) {
+//     return 'Hello ' + name;
+// }
+
+// console.log(greeting('Patience'));
+
+///Immediately Invoked Function Expressions (IIFE)
+
+// let sum = (function(a,b) {
+//     return a+b
+// })(2,3)
+
+// function sum (a,b){
+//     return a+b;
+// }
+
+// sum(2,3);
+
+// // console.log(sum);
+
+// ///ARROW FUNCTION
+// let add = (a, b) => a + b;
+//  console.log('arrow function: ' + add(3,2));
+// let sum = function(a,b){
+//     return a+b;
+// } 
+
+
+//////OPERATORS
+
+// let y = 10;
+// let x = 12;
+
+// let addition = x+y;
+// console.log('addition:'+ addition);
+
+// let substraction = x-y; // 12-10
+// console.log('substraction: '+substraction);
+
+// let multiplication = x*y;// 12 x 10
+
+// console.log('multiplication: ' + multiplication);
+
+// let division = x/2; // 12/2
+
+// console.log('division: '+ division);
+
+// let modulus = x%2 // 
+
+// console.log('modulus: ' + modulus);
+
+// //Increment
+// let increment = x++;
+// console.log('increment: ' +increment);
+// /////called secnd time
+// console.log(x);
+// // decrement
+// let decrement = y--;
+// console.log('decrement: '+ decrement);
+// /////called second time
+// console.log(y);
+
+
+//Exponentiation = **
+
+// let exponentiations = x**2; /// 12**2
+
+// console.log('exponentiation: '+exponentiations);
+
+
+////////IF STATEMENT///////
+
+// let numberOfFruit = 20; // type integer
+// let number = "20";// type string
+
+
+// if(numberOfFruit === number) {
+//     console.log('this is stricly equal');
+// }
+
+// if(numberOfFruit === number) {
+//     console.log('It is equal');  
+// }
+
+// if(numberOfFruit < 2) {
+//     console.log('the fruit is almost finished')
+// } else if(numberOfFruit === 20) {
+//     console.log('we have enough food')
+// } else {
+//     console.log('check your fruit basket');
+// }
+
+// if (numberOfFruit === number) {
+//     console.log('this is equal');
+// }  else if(numberOfFruit == number) {
+//     console.log('it is equal')
+// } else {
+//     console.log('check your conditions')
+// }
+//ternary operator
+//  (numberOfFruit===20)? console.log('this is equal') : console.log('check your conditions');
+// //SWicth case statment
+// let numberOfFruit = 20;
+// let fruits;
+
+// switch (true) {
+//     case (numberOfFruit < 2):
+//     fruits = "the fruit is almost finished";
+//     break;
+//     case(numberOfFruit === 11):
+//     fruits = " the number of fruit is 11";
+//     break;
+//     default:
+//     fruits = "check your basket";
+// }
+
+// console.log(fruits);
+
+
